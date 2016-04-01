36fa8c5310b146a9872ee4a1f802c73550a04cc4
Lobanov Aleksey <alexey.lobanov@ubrapaymentsystems.com>
2016-04-01 11:04:01 +0500

    initial commit

---
+++ ALVodkaNotification/ALVodkaNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALVodkaNotification
+//
+//  Created by Lobanov on 01.04.16.
+//  Copyright © 2016 Lobanov Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
